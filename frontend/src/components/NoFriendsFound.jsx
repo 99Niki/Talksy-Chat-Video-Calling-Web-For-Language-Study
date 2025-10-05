8a4e0bf3ddeddc1d7f657708ddaa441f23f7d278
99Niki <zhengliting378@gmail.com>
2025-10-05 10:21:35 -0400

    initial commit

---
+++ frontend/src/components/NoFriendsFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NoFriendsFound = () => {
+  return (
+    <div className="card bg-base-200 px-6 text-center">
+        <h3 className="font-semibold text-lg mb-2">Noo, you don't connect with any friends yet!</h3>
+        <p className="text-base-content opacity-70">Connect with language partners below to start practicing together!</p>
+    </div>
+  )
+}
+
+export default NoFriendsFound;
